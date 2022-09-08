bb0a44a40da1d8fa50881311e5d25497dffea6d5
Aleksandr Filippov <aleksandr@ax86.com>
2022-09-08 15:03:13 +0300

    Поменял объявление переменной a

--- a.c
+++ a.c
@@ -4,9 +4,9 @@ int main() {
 
    printf("Hello world!\n");
 
-   unsigned int a = 0x7FFFFFFF;
+   //unsigned int a = 0x7FFFFFFF;
    int b = 0x80000000;
-   //unsigned int a = 4294967295;
+   unsigned int a = 4294967295;
    printf("%u\n", a);
    printf("%d\n", b);
    printf("%d\n", a);
